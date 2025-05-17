b550e46825091ec50a4fdac70d864159e8d44ce1
HibaMbr <mb.hiba.18@gmail.com>
2025-05-17 13:40:10 +0200

    Hiba added POST endpoint in products

--- api/products.js
+++ api/products.js
@@ -14,8 +14,27 @@ export default () => {
         res.send('data')
     })
 
+    // POST
 
+    router.post('/', async (req, res) => {
+        const supabase = req.app.get('supabase')
 
+        const {data, error} = await supabase
+        .from("products")
+        .insert([req.body])
+        .select()
+
+        if (error) {
+            res.send({
+                code: 400,
+                message: error.message
+            })
+        }
+        res.send({
+            code: 200,
+            data
+        })
+    })
 
     return router
 }
